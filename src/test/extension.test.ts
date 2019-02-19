cc9a971fd58cf520eaad04569fe58c855a20620a
J. Heilingbrunner <j.heilingbrunner@gmail.com>
2019-02-19 14:29:44 +0100

    better promised test

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -10,22 +10,14 @@ import { promise_checkVersion } from '../gnupgpromises';
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 // import * as vscode from 'vscode';
-// import * as myExtension from '../extension';
 
 // Defines a Mocha test suite to group tests of similar kind together
 suite('Extension Tests', function() {
-
-
-  test('WarmUp Test A', function() {
-    let a = 'A';
-    assert.equal(a, 'A','Not A');
-  });
-
-  test('promise_checkVersion', function() {
-    promise_checkVersion().then(buff => {
-      const v = buff.toString();
-      assert.equal(v, 'A','Not A');
+  test('promise_checkVersion', () => {
+    return promise_checkVersion().then(buff => {
+      let version = '';
+      version = buff.toString();
+      assert.notEqual(version, '', 'No version');
     });
   });
-
 });
