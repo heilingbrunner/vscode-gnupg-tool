b2d6c0e85bd45ea9d9139aa42f47a3ab97ac7d8b
J. Heilingbrunner <j.heilingbrunner@gmail.com>
2019-02-19 13:46:17 +0100

    tests

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -5,6 +5,7 @@
 
 // The module 'assert' provides assertion methods from node
 import * as assert from 'assert';
+import { promise_checkVersion } from '../gnupgpromises';
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
@@ -15,11 +16,16 @@ import * as assert from 'assert';
 suite('Extension Tests', function() {
 
 
-  test('Test 1', function() {
-
-    //assert.equal('`num`', , '');
+  test('WarmUp Test A', function() {
+    let a = 'A';
+    assert.equal(a, 'A','Not A');
   });
 
-  
+  test('promise_checkVersion', function() {
+    promise_checkVersion().then(buff => {
+      const v = buff.toString();
+      assert.equal(v, 'A','Not A');
+    });
+  });
 
 });
