ab7e96a904ddb5baa3bc5f9afd3080370c90ecb9
J. Heilingbrunner <j.heilingbrunner@gmail.com>
2019-03-07 16:38:18 +0100

    tests

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -5,7 +5,7 @@
 
 // The module 'assert' provides assertion methods from node
 import * as assert from 'assert';
-import { promiseCheckVersion } from '../gnupgpromises';
+import { promiseCheckVersion, promiseListPublicKeys, promiseListSecretKeys } from '../gnupgpromises';
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
@@ -13,11 +13,21 @@ import { promiseCheckVersion } from '../gnupgpromises';
 
 // Defines a Mocha test suite to group tests of similar kind together
 suite('Extension Tests', function() {
-  test('promiseCheckVersion', () => {
-    return promiseCheckVersion().then(buff => {
-      let version = '';
-      version = buff.toString();
-      assert.notEqual(version, '', 'No version');
-    });
+  test('promiseCheckVersion', async () => {
+    const buff = await promiseCheckVersion();
+    let version = buff.toString();
+    assert.notEqual(version, '', 'No version');
+  });
+
+  test('promiseListPublicKeys', async () => {
+    const buff = await promiseListPublicKeys();
+    let keys = buff.toString();
+    assert.notEqual(keys, '', 'No list');
+  });
+
+  test('promiseListSecretKeys', async () => {
+    const buff = await promiseListSecretKeys();
+    let keys = buff.toString();
+    assert.notEqual(keys, '', 'No list');
   });
 });
